8515454db972e5f2fec3fbc7a684c6034ca47155
Dmitry H <developer.bulldozer@gmail.com>
2022-04-05 18:46:04 +0300

    Added new find method into the ProductController

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -3,11 +3,13 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Patch,
   Post,
 } from '@nestjs/common';
 import { ProductModel } from './product.model';
+import { FindProductDto } from './dto/find-product.dto';
 
 @Controller('product')
 export class ProductController {
@@ -22,4 +24,8 @@ export class ProductController {
 
   @Patch(':id')
   async patch(@Param('id') id: string, @Body() dto: ProductModel) {}
+
+  @HttpCode(200)
+  @Post()
+  async find(@Body() dto: FindProductDto) {}
 }
