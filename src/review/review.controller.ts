6c7f0e6750eeb60e72d5e0203b218fa2bc5fc83c
Dmitry H <developer.bulldozer@gmail.com>
2022-04-06 23:09:46 +0300

    Added logic to ReviewController methods

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,14 +1,39 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { Review } from './schemas/review.schema';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { ReviewService } from './review.service';
+import { REVIEW_NOT_FOUND } from './review.constants';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewService: ReviewService) {}
+
   @Post('create')
-  async create(@Body() dto: Omit<Review, '_id'>) {}
+  async create(@Body() dto: CreateReviewDto) {
+    return this.reviewService.create(dto);
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string) {
+    const deletedDoc = await this.reviewService.delete(id);
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND);
+    }
+    /**
+     * @dev If the deletion was successful, a code response "200" will be automatically returned.
+     */
+  }
 
   @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {}
+  async getByProduct(@Param('productId') productId: string) {
+    return this.reviewService.findByProductId(productId);
+  }
 }
